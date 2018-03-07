0fdee0fba0a2349cd99816426155cb0f1e0b5e79
east196 <2901180515@qq.com>
2018-03-07 17:09:24 +0800

    +fabfile

---
+++ fabfile.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from fabric.api import local, lcd
+
+
+def pull():
+    local("git pull https://github.com/East196/aiscrapy")
+
+
+def commit(doc="clean"):
+    local("git add . && git commit -m %s" % doc)
